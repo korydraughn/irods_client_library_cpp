6fd8e4ef2ca9d0afc09dabb5b88dab1553984912
Kory Draughn <korydraughn@renci.org>
2019-07-25 18:04:48 -0400

    Removed server source files.

--- src/server/api/include/rsModAVUMetadata.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef RS_MOD_A_V_U_METADATA_HPP
-#define RS_MOD_A_V_U_METADATA_HPP
-
-#include "modAVUMetadata.h"
-
-int rsModAVUMetadata( rsComm_t *rsComm, modAVUMetadataInp_t *modAVUMetadataInp );
-int _rsModAVUMetadata( rsComm_t *rsComm, modAVUMetadataInp_t *modAVUMetadataInp );
-
-#endif
--- src/server/api/include/rsRuleExecSubmit.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef RS_RULE_EXEC_SUBMIT_HPP
-#define RS_RULE_EXEC_SUBMIT_HPP
-
-#include "ruleExecSubmit.h"
-
-int rsRuleExecSubmit( rsComm_t *rsComm, ruleExecSubmitInp_t *ruleExecSubmitInp, char **ruleExecId );
-int _rsRuleExecSubmit( rsComm_t *rsComm, ruleExecSubmitInp_t *ruleExecSubmitInp );
-
-#endif
